56ba9fecd946e178b0fb6058bfd95fb41217188e
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-09-30 21:45:09 +0700

    tugas_10_golang
    ok

---
+++ tugas-10.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "time"
+
+func main(){
+  fmt.Println("Biodata Diri")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Nama: Mukhamat Jafar")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Hobby: Ngoding")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Tanggal Lahir: 05 March 1990")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Terima Kasih")
+}
